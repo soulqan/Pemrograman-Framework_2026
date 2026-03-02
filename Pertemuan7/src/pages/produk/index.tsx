35899306c290ebc201d92f38016528481dab4e48
soulqan <soultan.agnar@gmail.com>
2026-03-02 17:08:52 +0700

    P7_Praktikum1-3

---
+++ Pertemuan7/src/pages/produk/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TampilanProduk from "../views/product";
+
+const kategori = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+  // console.log("products:", products);
+  
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     push("/auth/login");
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    fetch("/api/produk")
+      .then((response) => response.json())
+      .then((responsedata) => {
+        setProducts(responsedata.data);
+        // console.log("Data produk:", responsedata.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching produk:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+
+export default kategori;
