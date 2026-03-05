97717ef1bd34233df84e05c5540f0114c458a584
soulqan <soultan.agnar@gmail.com>
2026-03-05 20:34:19 +0700

    P9_Praktikum

---
+++ Pertemuan9/src/pages/produk/static.tsx
@@ -0,0 +1,27 @@
+import TampilanProduk from "../../views/product";
+import { ProductType } from "../../types/Product.type";
+
+const halamanProdukStatic = (props: { products: ProductType[] }) => {
+  const { products } = props;
+
+  return (
+    <div>
+      <h1>Halaman Produk Static</h1>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+
+export default halamanProdukStatic;
+
+export async function getStaticProps() {
+  const res = await fetch("http://127.0.0.1:3000/api/produk");
+
+  const response: { data: ProductType[] } = await res.json();
+
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
---
+++ Pertemuan9/src/views/product/index.tsx
@@ -0,0 +1,53 @@
+import styles from "../../pages/produk/product.module.scss";
+
+type ProductType = {
+    id: string;
+    nama: string;
+    price: number;
+    image: string;
+    size: string;
+    category: string;
+};
+
+const TampilanProduk = ({ products }: { products: ProductType[] }) => {
+    return (
+        <div className={styles.produk}>
+            <h1 className={styles.produk__title}>Daftar Produk</h1>
+            <div className={styles.produk__content}>
+                {products.length > 0 ? (
+                    <>
+                     {products.map((product: ProductType) => (
+                    <div key={product.id} className={styles.produk__content__item}>
+                        <div className={styles.produk__content__item__image}>
+                            <img src={product.image} alt={product.nama} width={200} />
+                        </div>
+                        <h4 className={styles.produk__content__item__name}>
+                            {product.nama}
+                        </h4>
+                        <p className={styles.produk__content__item__category}>
+                            Kategori: {product.category}
+                        </p>
+                        <p className={styles.produk__content__item__price}>
+                            Rp {product.price.toLocaleString("id-ID")}
+                        </p>
+                        <p className={styles.produk__content__item__size}>
+                            Ukuran: {product.size}
+                        </p>
+                    </div>
+                ))}
+                </>
+                ) :(<div className={styles.produk__content__skeleton}>
+                    <div className={styles.produk__content__skeleton__image}></div>
+                    <div className={styles.produk__content__skeleton__name}></div>
+                    <div className={styles.produk__content__skeleton__category}></div>
+                    <div className={styles.produk__content__skeleton__price}></div>
+                    <div className={styles.produk__content__skeleton__size}></div>
+                </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default TampilanProduk;
+
